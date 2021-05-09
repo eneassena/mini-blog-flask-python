2c049842c9c50aed576286d4903106d42080d752
eneas200 <desenvolvedorneas2000@gmail.com>
2021-05-09 17:49:16 -0300

    novas implementaçoes

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,4 +13,13 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+   
+   
+   
+
+from app import routes, models, errors
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,19 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditorProfile
 from flask_login import current_user, login_user
 from app.models import User
 from flask_login import logout_user, login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 # rota de deslogar da conta
 @app.route('/logout')
@@ -64,8 +73,7 @@ def login():
 		next_page = request.args.get('next')
 		if not next_page or url_parse(next_page).netloc != '':
 			next_page = url_for('index')
-		next_page = str(next_page).replace('/', '')
-		flash(user.username)
+		next_page = str(next_page).replace('/', '') 
 		return redirect(url_for(next_page))
 	return render_template('login.html', title="Sigin In", form=form)
 
@@ -81,5 +89,22 @@ def user(username):
 	return render_template('user.html', user=user, posts=posts)
     
      
-
+# rota de edição de profiles
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+	form = EditorProfile()
+	if form.validate_on_submit():
+		current_user.username = form.username.data
+		current_user.about_me = form.about_me.data
+		db.session.commit()
+		flash('Your changes have been saved.')
+		return redirect(url_for('edit_profile'))
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.about_me.data = current_user.about_me
+		return render_template('edit_profile.html', title='Edit Profile', form=form)
+	else:
+		flash(f"Seu Texto tem tamanho de {len(form.about_me.data)} e passa do limite do campo about que permite 150 caracteres")
+		return redirect(url_for('edit_profile'))
 
